726fa548af8b4523476a796808dcfb5ed335434d
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-12 01:50:09 +0300

    test

--- index.php
+++ index.php
@@ -13,63 +13,72 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Initialize the plugin
-function shipping_plugin_init() {
-    // Add your initialization code here
-    if ( ! class_exists( 'WC_Your_Shipping_Method' ) ) {
-        class WC_Your_Shipping_Method extends WC_Shipping_Method {
-            /**
-             * Constructor for your shipping class
-             *
-             * @access public
-             * @return void
-             */
-            public function __construct() {
-                $this->id                 = 'your_shipping_method';
-                $this->title       = __( 'Your Shipping Method' );
-                $this->method_description = __( 'Description of your shipping method' ); // 
-                $this->enabled            = "yes"; // This can be added as an setting but for this example its forced enabled
-                $this->init();
-            }
-    
-            /**
-             * Init your settings
-             *
-             * @access public
-             * @return void
-             */
-            function init() {
-                // Load the settings API
-                $this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
-                $this->init_settings(); // This is part of the settings API. Loads settings you previously init.
-    
-                // Save settings in admin if you have any defined
-                add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
-            }
-    
-            /**
-             * calculate_shipping function.
-             *
-             * @access public
-             * @param mixed $package
-             * @return void
-             */
-            public function calculate_shipping( $package ) {
-                // This is where you'll add your rates
-            }
-        }
-    }
-    
-        
-}
-add_action('init', 'shipping_plugin_init');
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
-// Example function to calculate shipping cost
-function calculate_shipping_cost($weight, $distance) {
-    $base_cost = 5.00; // Base cost in USD
-    $cost_per_kg = 1.50; // Cost per kilogram
-    $cost_per_km = 0.10; // Cost per kilometer
+	function your_shipping_method_init() {
+		if ( ! class_exists( 'WC_Your_Shipping_Method' ) ) {
+			class WC_Your_Shipping_Method extends WC_Shipping_Method {
+				/**
+				 * Constructor for your shipping class
+				 *
+				 * @access public
+				 * @return void
+				 */
+				public function __construct() {
+					$this->id                 = 'your_shipping_method'; // Id for your shipping method. Should be uunique.
+					$this->method_title       = __( 'Your Shipping Method' );  // Title shown in admin
+					$this->method_description = __( 'Description of your shipping method' ); // Description shown in admin
 
-    return $base_cost + ($weight * $cost_per_kg) + ($distance * $cost_per_km);
+					$this->enabled            = "yes"; // This can be added as an setting but for this example its forced enabled
+					$this->title              = "My Shipping Method"; // This can be added as an setting but for this example its forced.
+
+					$this->init();
+				}
+
+				/**
+				 * Init your settings
+				 *
+				 * @access public
+				 * @return void
+				 */
+				function init() {
+					// Load the settings API
+					$this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
+					$this->init_settings(); // This is part of the settings API. Loads settings you previously init.
+
+					// Save settings in admin if you have any defined
+					add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+				}
+
+				/**
+				 * calculate_shipping function.
+				 *
+				 * @access public
+				 * @param array $package
+				 * @return void
+				 */
+				public function calculate_shipping( $package = array() ) {
+					$rate = array(
+						'label' => $this->title,
+						'cost' => '10.99',
+						'calc_tax' => 'per_item'
+					);
+
+					// Register the rate
+					$this->add_rate( $rate );
+				}
+			}
+		}
+	}
+
+	add_action( 'woocommerce_shipping_init', 'your_shipping_method_init' );
+
+	function add_your_shipping_method( $methods ) {
+		$methods['your_shipping_method'] = 'WC_Your_Shipping_Method';
+		return $methods;
+	}
+
+	add_filter( 'woocommerce_shipping_methods', 'add_your_shipping_method' );
 }
-?>
+
+	
