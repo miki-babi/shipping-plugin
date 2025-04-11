48ea81232704054ea69ac1ad951217fa5dfac4e4
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-12 01:47:17 +0300

    test

--- index.php
+++ index.php
@@ -16,6 +16,51 @@ if (!defined('ABSPATH')) {
 // Initialize the plugin
 function shipping_plugin_init() {
     // Add your initialization code here
+    if ( ! class_exists( 'WC_Your_Shipping_Method' ) ) {
+        class WC_Your_Shipping_Method extends WC_Shipping_Method {
+            /**
+             * Constructor for your shipping class
+             *
+             * @access public
+             * @return void
+             */
+            public function __construct() {
+                $this->id                 = 'your_shipping_method';
+                $this->title       = __( 'Your Shipping Method' );
+                $this->method_description = __( 'Description of your shipping method' ); // 
+                $this->enabled            = "yes"; // This can be added as an setting but for this example its forced enabled
+                $this->init();
+            }
+    
+            /**
+             * Init your settings
+             *
+             * @access public
+             * @return void
+             */
+            function init() {
+                // Load the settings API
+                $this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
+                $this->init_settings(); // This is part of the settings API. Loads settings you previously init.
+    
+                // Save settings in admin if you have any defined
+                add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+            }
+    
+            /**
+             * calculate_shipping function.
+             *
+             * @access public
+             * @param mixed $package
+             * @return void
+             */
+            public function calculate_shipping( $package ) {
+                // This is where you'll add your rates
+            }
+        }
+    }
+    
+        
 }
 add_action('init', 'shipping_plugin_init');
 
