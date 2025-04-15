157939a652683a922b68c09084a4b33ac0ae4734
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-15 22:15:20 +0300

    calculate weight

--- includes/class-express-delivery.php
+++ includes/class-express-delivery.php
@@ -38,41 +38,37 @@ class Express_Delivery extends WC_Shipping_Method {
     }
 
     public function calculate_shipping($package = []) {
-
-        $weight = $this->get_package_weight($package);
-
-        
-
+        $weight = 0;
+    
+        if (!empty($package['contents'])) {
+            foreach ($package['contents'] as $item) {
+                if (isset($item['data']) && $item['data']->has_weight()) {
+                    $weight += floatval($item['data']->get_weight()) * $item['quantity'];
+                }
+            }
+        }
+    
+        $base_cost = floatval($this->get_option('base_cost'));
+    
         if ($weight > 10) {
-            $base_cost = $this->get_option('base_cost') + 20; // Additional cost for heavy packages
+            $base_cost += 20;
         } else if ($weight > 5) {
-            $base_cost = $this->get_option('base_cost') + 10; // Additional cost for medium packages
-        } else if ($weight > 0) {
-            $base_cost = $this->get_option('base_cost'); // Base cost for light packages
-        } else {
-            $base_cost = $this->get_option('base_cost');
+            $base_cost += 10;
         }
-        
-        floatval($this->get_option('base_cost'));
+    
         $per_km = floatval($this->get_option('cost_per_km'));
-        $distance_km = 0;
-
-        // Get distance from cookie
-        if (isset($_COOKIE['delivery_distance'])) {
-            $distance_km = floatval($_COOKIE['delivery_distance']);
-        }
-
-        // Fallback if cookie not set
+        $distance_km = isset($_COOKIE['delivery_distance']) ? floatval($_COOKIE['delivery_distance']) : 1;
         if ($distance_km <= 0) {
-            $distance_km = 1; // Default minimum distance
+            $distance_km = 1;
         }
-
+    
         $cost = $base_cost + ($per_km * $distance_km);
-
+    
         $this->add_rate([
             'id' => $this->id,
             'label' => $this->title,
             'cost' => round($cost, 2),
         ]);
     }
+    
 }
