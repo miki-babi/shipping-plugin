e751cce86f787e96cf0f897c7368e9b41c696a5a
miki-babi <mikiyasshiferaw@gmail.com>
2025-08-25 22:24:29 +0300

    rules

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Shipping Plugin
  * Description: A plugin for managing shipping functionality.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: mikiyas
  * Author URI: https://t.me/mikiyas_sh
  * License: GPL-2.0+
